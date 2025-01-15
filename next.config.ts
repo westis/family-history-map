b1c3b9ae2057e6d4877aa58544b3817c101b4a80
Daniel Westergren <westis@gmail.com>
2025-01-15 22:41:53 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   basePath: "/family-history-map",
   images: {
     unoptimized: true,
   },
-  distDir: "out",
   trailingSlash: true,
   env: {
     NEXT_PUBLIC_MAPTILER_KEY: process.env.NEXT_PUBLIC_MAPTILER_KEY,
