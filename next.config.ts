6da58ab770a345c4be0a697d6648a12d06915b6a
Daniel Westergren <westis@gmail.com>
2025-01-15 22:17:04 +0100

    fix?

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enable static exports
-  basePath: "/family-history-map", // Replace with your repository name
+  output: "export",
+  basePath: "/family-history-map",
   images: {
     unoptimized: true,
   },
   distDir: "out",
   trailingSlash: true,
   env: {
-    NEXT_PUBLIC_LANTMATERIET_CONSUMER_KEY:
-      process.env.NEXT_PUBLIC_LANTMATERIET_CONSUMER_KEY,
-    NEXT_PUBLIC_LANTMATERIET_CONSUMER_SECRET:
-      process.env.NEXT_PUBLIC_LANTMATERIET_CONSUMER_SECRET,
+    NEXT_PUBLIC_MAPTILER_KEY: process.env.NEXT_PUBLIC_MAPTILER_KEY,
+  },
+  assetPrefix: "/family-history-map/",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
   },
 };
 
