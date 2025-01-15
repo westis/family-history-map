0522a0f5e0fec155caf3ec0f7a393418c65ba0e5
Daniel Westergren <westis@gmail.com>
2025-01-15 22:29:39 +0100

    fix?

--- components/ClientFamilyMap.tsx
+++ components/ClientFamilyMap.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 
-const FamilyMap = dynamic(() => import("@/components/FamilyMap"), {
+const FamilyMap = dynamic(() => import("./FamilyMap"), {
   ssr: false,
   loading: () => (
     <div className="flex items-center justify-center h-screen">
